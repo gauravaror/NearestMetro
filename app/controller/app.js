056b11e9fa0847c5ec7b690d8556fcc610e83e01
Gaurav Arora <gauravarora.daiict@gmail.com>
2014-08-11 18:44:46 +0530

    Initial angular js setup done

---
+++ app/controller/app.js
@@ -0,0 +1,6 @@
+var app = angular.module("NearestMetro", []);
+
+app.controller('ngAppDemoController',[ '$scope', function ($scope) {
+    $scope.name = "Nehru Place"
+    //The body of demo controller
+}]);
