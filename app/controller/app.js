45f5c11d69c31171c63457c7285667f593c6f29f
Gaurav Arora <gauravarora.daiict@gmail.com>
2014-08-11 20:39:10 +0530

    Added maps using map api

--- app/controller/app.js
+++ app/controller/app.js
@@ -3,4 +3,7 @@ var app = angular.module("NearestMetro", []);
 app.controller('ngAppDemoController',[ '$scope', function ($scope) {
     $scope.name = "Nehru Place"
     //The body of demo controller
+    $scope.decodeAddress = function () {
+        codeAddress($scope.name);
+    }
 }]);
